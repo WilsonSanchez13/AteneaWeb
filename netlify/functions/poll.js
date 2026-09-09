bda983b55b15eac18b9533ae5572f5480bd909eb
Wilson Sanchez <109751152+WilsonSanchez13@users.noreply.github.com>
2026-09-09 13:00:15 -0500

    Update poll.js

--- netlify/functions/poll.js
+++ netlify/functions/poll.js
@@ -4,6 +4,7 @@ const STORE_NAME = "atenea-data";
 const KEY = "poll";
 
 exports.handler = async (event) => {
+    connectLambda(event);
   const store = getStore(STORE_NAME);
 
   if (event.httpMethod === "GET") {
